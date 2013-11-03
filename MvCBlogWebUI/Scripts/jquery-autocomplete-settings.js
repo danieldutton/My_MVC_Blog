e2648c77d046ee89cd037874a6982902a0bce7a6
Daniel Dutton <danielbdutton@googlemail.com>
2013-11-03 22:20:09 +0000

    Added JavaScript autocomplete with settings

---
+++ MvCBlogWebUI/Scripts/jquery-autocomplete-settings.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function() {
+    $("#Categories").autocomplete({
+        source: function(request, response) {
+            $.ajax({
+                url: "/Home/AutoCompleteCategory",
+                type: "POST",
+                dataType: "json",
+                data: { term: request.term, maxRows: 12 },
+                success: function(data) {
+                    response($.map(data, function(item) {
+                        return { label: item, value: item };
+                    }));
+
+                }
+            });
+        },
+    });
+});
+
