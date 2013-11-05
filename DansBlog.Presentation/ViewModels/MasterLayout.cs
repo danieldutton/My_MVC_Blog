a0c67e32ab20f2d19b3772a8bbfb3b01a53d2b99
Daniel Dutton <danielbdutton@googlemail.com>
2013-11-05 21:01:22 +0000

    Solution Project Folders rename
    Renamed project folders in file explorer to match project names

---
+++ DansBlog.Presentation/ViewModels/MasterLayout.cs
@@ -0,0 +1,16 @@
+﻿using DansBlog.Model.Entities;
+using DansBlog.Model.Partials;
+using System.Collections.Generic;
+using DansBlog.Services.Archiving.ViewModel;
+
+namespace DansBlog.Presentation.ViewModels
+{
+    public class MasterLayout
+    {
+        public List<Archive> ArchivedMonths { get; set; }
+        
+        public List<Category> Categories { get; set; }
+        
+        public Quote QuoteOfTheDay { get; set; }
+    }
+}
