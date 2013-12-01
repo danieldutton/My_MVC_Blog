53ebfd6ab80eab3ce6e74b0294fbde189225c67d
Daniel Dutton <danielbdutton@googlemail.com>
2013-12-01 15:54:04 +0000

    Full code coverage for Tag class

--- DansBlog.UnitTests/Model/Entities/Tag_Should.cs
+++ DansBlog.UnitTests/Model/Entities/Tag_Should.cs
@@ -1,11 +1,34 @@
 ﻿using DansBlog.Model.Entities;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DansBlog._UnitTests.Model.Entities
 {
     [TestFixture]
     public class Tag_Should
     {
+        [Test]
+        public void Name_FailModelValidationIfNameIsMissing()
+        {
+            var sut = new Tag {Name = null};
+
+            int errorCount = Mother.ValidateModel(sut).Count;
+
+            Assert.AreEqual(1, errorCount);
+        }
+
+        [Test]
+        public void Name_GenerateTheCorrectErrorMessageIfNameIsMissing()
+        {
+            var sut = new Tag { Name = null };
+
+            IList<ValidationResult> result = Mother.ValidateModel(sut);
+            string errorMessage = result[0].ErrorMessage;
+
+            Assert.AreEqual("Name is required", errorMessage);
+        }
+
         [Test]
         public void Equals_ReturnTrueIfTwoCategoriesHaveIdenticalNameValues()
         {
