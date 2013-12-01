770de05717a5d23e4ca159f8da5d311d301b3264
Daniel Dutton <danielbdutton@googlemail.com>
2013-12-01 14:55:49 +0000

    Added unit test for Post.cs
    All tests pass

---
+++ DansBlog.UnitTests/Model/Entities/PostShould.cs
@@ -0,0 +1,41 @@
+﻿using DansBlog.Model.Entities;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace DansBlog.UnitTests.Model.Entities
+{
+    [TestFixture]
+    public class PostShould
+    {
+        [Test]
+        public void ModeratedCommentCount_ReturnTheCorrectCountForModeratedComments()
+        {
+            var sut = new Post
+                {
+                    Comments = new List<Comment>
+                        {
+                            new Comment{HasBeenModerated = true}, 
+                            new Comment{HasBeenModerated = true},
+                            new Comment {HasBeenModerated = false},
+                        }
+                };
+
+            int result = sut.ModeratedCommentCount;
+
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void ModeratedCommentCount_Return0IfPostCommentsArNull()
+        {
+            var sut = new Post
+                {
+                    Comments = null,
+                };
+
+            int result = sut.ModeratedCommentCount;
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
