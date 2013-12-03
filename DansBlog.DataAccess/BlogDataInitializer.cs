ebbe2a86e81e458ee8d96ae235bce15f212988cb
Daniel Dutton <danielbdutton@googlemail.com>
2013-12-03 16:51:55 +0000

    Misc alterations

--- DansBlog.DataAccess/BlogDataInitializer.cs
+++ DansBlog.DataAccess/BlogDataInitializer.cs
@@ -1,7 +1,4 @@
-﻿using DansBlog.Model.Entities;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 
 namespace DansBlog.DataAccess
 {
@@ -9,37 +6,7 @@ namespace DansBlog.DataAccess
     {
         protected override void Seed(BlogDbContext context)
         {
-            var postsList = new List<Post>
-            {               
-            new Post
-            {
-               Title = "Blog 3 Title Category Random Category",
-               PublishDate = new DateTime(2012, 11, 23),
-               Content = "Content for Blog 3 woohoo Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nisl dolor, volutpat a vehicula et, scelerisque non purus. Vestibulum at semper purus. Nunc gravida mollis posuere. Mauris vel condimentum diam. Morbi at pulvinar enim. Vivamus et neque sed ipsum tincidunt ultricies nec sit amet eros",
-               Author = "Daniel Dutton",
-               Comments = new List<Comment>
-            {
-               new Comment
-               {
-                  Author = "Daniel Dutton",
-                  Content = "Blog 3 Comment 3",
-                  Email = "danielbdutton@hotmail.co.uk",
-                  CreationTime = DateTime.Now,
-                  HasBeenModerated = true,
-                }    
-             },
-              Categories = new List<Category>
-                              {
-                                  new Category{Name = "Random Category"},
-                              }
-            },
-
-           };
-
-            foreach (var post in postsList)
-            {
-                context.Posts.Add(post);
-            }
+            //seed data here
 
             base.Seed(context);
         }
